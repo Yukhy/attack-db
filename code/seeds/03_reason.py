47bff4a8b02f1229af7c45bcc8976deb48bfc8df
yukhy <yukhy0119@gmail.com>
2023-03-08 11:37:06 +0900

    テーブル名の変更

--- code/seeds/03_reason.py
+++ code/seeds/03_reason.py
@@ -3,25 +3,22 @@ from settings import Session
 from models import Tactic, Technique, Reason
 from mitreattack.stix20 import MitreAttackData
 
-try:
-    Reason.num_of_row()
-    print("Reason table already exists.")
-except exc.SQLAlchemyError:
-    mitre_attack_data = MitreAttackData("/resources/enterprise-attack.json")
-    techniques = mitre_attack_data.get_techniques(remove_revoked_deprecated=True)
 
-    for technique in techniques:
-        parent_tactics = list()
-        for kill_chain_phase in technique.kill_chain_phases:
-            parent_tactics.append(kill_chain_phase["phase_name"])
+mitre_attack_data = MitreAttackData("/resources/enterprise-attack.json")
+techniques = mitre_attack_data.get_techniques(remove_revoked_deprecated=True)
 
-        technique_id = Technique.get_id_by_name(technique.name)
-        for tactic in parent_tactics:
-            tactic_name = tactic.replace("-", " ") # ex.) "privilege-escalation" -> "privilege escalation"
-            record = Reason()
-            record.tactic_id = Tactic.get_id_by_name(tactic_name)
-            record.technique_id = technique_id
-            Session.add(record)
-    Session.commit()
-    Session.close()
-    print("Reason table created.")
+for technique in techniques:
+    parent_tactics = list()
+    for kill_chain_phase in technique.kill_chain_phases:
+        parent_tactics.append(kill_chain_phase["phase_name"])
+
+    technique_id = Technique.get_id_by_name(technique.name)
+    for tactic in parent_tactics:
+        tactic_name = tactic.replace("-", " ") # ex.) "privilege-escalation" -> "privilege escalation"
+        record = Reason()
+        record.tactic_id = Tactic.get_id_by_name(tactic_name)
+        record.technique_id = technique_id
+        Session.add(record)
+Session.commit()
+Session.close()
+print("Reason table created.")
